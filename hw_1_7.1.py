da1554dd86c4f65e9d82ef2fa65ec27749017cce
aloster <asket2013@ya.ru>
2022-08-25 19:26:20 +1000

    Complete work 1_7.1

--- hw_1_7.1.py
+++ hw_1_7.1.py
@@ -2,6 +2,7 @@ def cookbook():
     cook_book = {}
     foods = []
     j = 0
+    list_ingr = ['ingredient_name', 'quantity', 'measure']
     with open('recipes.txt', 'r') as f:
         read_book = f.read().splitlines()
     foods.append(j)
@@ -12,8 +13,8 @@ def cookbook():
     for i in foods:
         cook_book[read_book[i]] = []
         for k in range(0, int(read_book[i + 1])):
-            j = read_book[i + 2 + k]
-            cook_book[read_book[i]].append({j})
+            ingredients = read_book[i + 2 + k].split(' | ')
+            cook_book[read_book[i]].append(dict(zip(list_ingr, ingredients)))
 
     return cook_book
 
