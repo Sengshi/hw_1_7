35bb8e189e1353cdf857262e6b8ed2dd6ebae57c
aloster <asket2013@ya.ru>
2022-08-25 20:14:43 +1000

    Обновлен вывод по задаче 1_7.1

--- hw_1_7.1.py
+++ hw_1_7.1.py
@@ -14,7 +14,8 @@ def cookbook():
         cook_book[read_book[i]] = []
         for k in range(0, int(read_book[i + 1])):
             ingredients = read_book[i + 2 + k].split(' | ')
-            cook_book[read_book[i]].append(dict(zip(list_ingr, ingredients)))
+            cook_book[read_book[i]].append({list_ingr[0]: ingredients[0],
+                                            list_ingr[1]: int(ingredients[1]), list_ingr[2]: ingredients[2]})
 
     return cook_book
 
@@ -24,10 +25,10 @@ def get_shop_list_by_dishes(dishes, person_count):
     for i in dishes:
         for j in cookbook()[i]:
             if j['ingredient_name'] not in get_shop:
-                get_shop[j['ingredient_name']] = {'quantity': int(j['quantity']) * person_count,
+                get_shop[j['ingredient_name']] = {'quantity': j['quantity'] * person_count,
                                                   'measure': j['measure']}
             else:
-                get_shop[j['ingredient_name']]['quantity'] += int(j['quantity']) * person_count
+                get_shop[j['ingredient_name']]['quantity'] += j['quantity'] * person_count
     return get_shop
 
 
